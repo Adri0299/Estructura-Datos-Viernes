71b75671ba86b775f3a98d05bc7561de3fc0d871
Adri0299 <113482972+Adri0299@users.noreply.github.com>
2022-09-23 18:57:03 -0600

    created
    Semana2

---
+++ src/main/java/com/mycompany/viernes/Viernes.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.viernes;
+
+/**
+ *
+ * @author Adriana
+ */
+public class Viernes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
