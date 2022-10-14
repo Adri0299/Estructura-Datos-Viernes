d804050c0080aa09b39703eb913ff451ac70f7b9
Adri0299 <113482972+Adri0299@users.noreply.github.com>
2022-10-13 22:17:04 -0600

    Semana 3

---
+++ Estructura Datos/src/TrabajoClase/Semana3/Main.java
@@ -0,0 +1,14 @@
+package TrabajoClase.Semana3;
+
+public class Main {
+
+    public static void main(String[] args) {
+  Stack<Integer> Pila = new Stack<Integer>();
+        Pila.push(1);
+        Pila.push(2);
+        Pila.push(3);
+        System.out.println(Pila.pop());
+        System.out.println(Pila.pop());
+        System.out.println(Pila.pop());
+    }
+}
